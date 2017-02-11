495b6f2c1e61563308d55ab5eccf70031015e4ad
Patrick Tener <Patrick Tener>
2017-02-10 19:53:49 -0500

    My test commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	//Patrick's Comment
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
