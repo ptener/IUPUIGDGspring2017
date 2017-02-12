2a82a654ed4b06df17516df5fece468006df1343
NobleAbe <linkthayer@sbcglobal.net>
2017-02-12 18:06:29 -0500

    added base for path/nodes

---
+++ Assets/scrip.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrip : MonoBehaviour {
+
+	public GameObject nodeUser;
+	public Transform[] nodes;
+	public Transform currentNode;
+	public double speed;
+	// Use this for initialization
+	void Start () {
+		nodes = new Transform[transform.childCount];
+		int i = 0;
+
+		foreach (Transform t in transform) {
+			nodes [i++] = t;
+		}
+
+		currentNode = nodes [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveTowards (currentNode);
+
+	}
+
+	void moveTowards(Transform currentNode)
+	{
+		float tempx, tempy, tempz;
+		tempx = currentNode.transform.position.x - this.transform.position.x;
+		tempy = currentNode.transform.position.y - this.transform.position.y;
+		tempz = currentNode.transform.position.x - this.transform.position.z;
+
+		if (tempx != 0) {
+			this.;
+			this.transform.position.x + speed;
+		}
+		if (tempy != 0) {
+			this.transform.position.y + speed;
+		}
+		if (tempz != 0) {
+			this.transform.position.z + speed;
+		}
+	}
+}
