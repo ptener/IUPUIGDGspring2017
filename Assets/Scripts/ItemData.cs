bb03baf081680062f363bdef6ecfd265a488d19f
Patrick Tener <Patrick Tener>
2017-02-24 19:16:56 -0500

    Inventory slots with an add item function

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour {
+	public Item item;
+	public int amount;
+
+	//default constructor
+	//called when itemData is created
+	public ItemData()
+	{
+		amount = 1;
+	}
+}
