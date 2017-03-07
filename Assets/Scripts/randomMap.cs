a12d1cd360ff3f5a2fc4708f544e901c5ebbdde9
lrthayer <lrthayer@iupui.edu>
2017-03-06 19:08:59 -0500

    caring about clean code
    made functions for every repeated step for the map creation process,
    making it easier to read and comprehend what is happening.

--- Assets/Scripts/randomMap.cs
+++ Assets/Scripts/randomMap.cs
@@ -15,6 +15,16 @@ public class randomMap : MonoBehaviour
 	int hallwayIndex = 0;
 	int randomSize;
 	int randomChoice;
+	List<string> usedRoomDirections = new List<string> ();
+	List<roomScript> rooms = new List<roomScript>();
+	int roomNodeOffsets = 0;
+	int totalRoomNodeOffsets = 0;
+	Transform platform;
+	Vector3 sizeofCurrentPlane;
+	Transform Room1;
+	Transform room1Plane;
+	Transform temp;
+
 	// Use this for initialization
 	void Start () {
 
@@ -24,12 +34,9 @@ public class randomMap : MonoBehaviour
 		Transform Corner2 = transform.GetChild (3);
 		Transform Corner3 = transform.GetChild (4);
 		Transform Corner4 = transform.GetChild (5);
-		Transform Room1 = transform.GetChild(6);
-		Transform room1Plane = transform.GetChild(6);
-		List<roomScript> rooms = new List<roomScript>();
-		List<string> usedRoomDirections = new List<string> ();
-		int roomNodeOffsets = 0;
-		int totalRoomNodeOffsets = 0;
+		Room1 = transform.GetChild(6);
+		room1Plane = transform.GetChild(6);
+
 
 		//init rooms
 		for (int i=0; i < numberofRooms; i++)
@@ -38,231 +45,43 @@ public class randomMap : MonoBehaviour
 		randomSize = Random.Range (10, lengthofCorridors);
 		Transform[] currentHallway = { straightHallway, leftHallway, Corner1, Corner2 , Corner3, Corner4};
 
-		Vector3 sizeofCurrentPlane = straightHallway.GetComponent<Renderer> ().bounds.size;
+		sizeofCurrentPlane = straightHallway.GetComponent<Renderer> ().bounds.size;
 
-		Transform temp = transform;
+		temp = transform;
 		Ray wallRay = new Ray (this.transform.GetChild (0).transform.position, Vector3.forward);
 
 		//init transform so they are not empty, but we don't use our own transform for these vars.
-		Transform platform = transform;
-		Transform platformNode = transform;
+		platform = transform;
 
 		for (int i = 0; i < numberofRooms; i++) {
 			totalRoomNodeOffsets += roomNodeOffsets;
 			roomNodeOffsets = 0;
 			int dividedCorner = numberofCorners / rooms [i].numOfPaths;
 			//for each path get a direction that hasn't been used yet
-			getDirection();
 			while (usedRoomDirections.Contains (direction)) {
-				Debug.Log ("used room directions: " +usedRoomDirections);
+				Debug.Log("TEST");
 				getDirection ();
 			}
-			Debug.Log ("Divided corner: " + dividedCorner);
-			Debug.Log ("number of Paths: " + rooms [i].numOfPaths);
 			for (int l = 0; l < rooms [i].numOfPaths; l++) {
 				
 				for (int k = 0; k < dividedCorner; k++) {
 					randomSize = Random.Range (4, lengthofCorridors);
 					for (int j = 0; j < randomSize - 1; j++) {
+						
 						if (j == 0 && i == 0)
 							platform = currentHallway [hallwayIndex];
 						else
 							platform = transform.GetChild (transform.childCount - 1 - totalRoomNodeOffsets);
-
-							
-						sizeofCurrentPlane = platform.transform.GetComponent<Renderer> ().bounds.size;
-
-						//if sizeofCurrentPlane is a room half it
-						if (sizeofCurrentPlane.x > 10) {
-							
-							sizeofCurrentPlane = sizeofCurrentPlane / 2f;
-							if (direction == "vert+")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x + sizeofCurrentPlane.x + 5f, platform.position.y, platform.position.z), Quaternion.identity);
-							else if (direction == "vert-")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x - sizeofCurrentPlane.x - 5f, platform.position.y, platform.position.z), Quaternion.identity);
-							else if (direction == "hort-")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z + 5f), currentHallway [hallwayIndex].localRotation);
-							else if (direction == "hort+")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z - 5f), currentHallway [hallwayIndex].localRotation);
-							temp.transform.SetParent (this.transform);
-						} else {
-							if (direction == "vert+")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x + sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
-							else if (direction == "vert-")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x - sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
-							else if (direction == "hort-")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z), currentHallway [hallwayIndex].localRotation);
-							else if (direction == "hort+")
-								temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z), currentHallway [hallwayIndex].localRotation);
-							temp.transform.SetParent (this.transform);
-						}
-						roomNodeOffsets++;
+						
+						addPanel(currentHallway);
 					}
+					getDirection();
 
-					randomChoice = Random.Range (0, 3);
-
-
-
-					lastdirection = direction;
-
-					if (direction.Contains ("hort") && randomChoice == 0) {
-
-						hallwayIndex = 0;
-
-						if (direction.Contains ("hort+")) {
-							cornerPiece = 5;
-						} else {
-							cornerPiece = 4;
-						}
-
-						direction = "vert+";
-
-					} else if (direction.Contains ("hort") && randomChoice == 1) {
-
-						hallwayIndex = 0;
-
-						if (direction.Contains ("hort+")) {
-							cornerPiece = 3;
-						} else {
-							cornerPiece = 2;
-						}
-
-						direction = "vert-";
-					} else if (direction.Contains ("hort") && randomChoice == 2) {
-						cornerPiece = 1;
-					} else if (direction.Contains ("vert") && randomChoice == 0) {
-
-						hallwayIndex = 1;
-
-						if (direction.Contains ("vert+")) {
-							cornerPiece = 2;
-						} else {
-							cornerPiece = 4;
-						}
-
-						direction = "hort+";
-
-					} else if (direction.Contains ("vert") && randomChoice == 1) {
-
-						hallwayIndex = 1;
-
-
-						if (direction.Contains ("vert+")) {
-							cornerPiece = 3;
-						} else {
-							cornerPiece = 5;
-						}
-
-						direction = "hort-";
-					} else if (direction.Contains ("vert") && randomChoice == 2) {
-						//do nothing, keep going in same direction
-						cornerPiece = 0;
-					}
-
-
-					//add the last piece of tile here
-					platform = transform.GetChild (transform.childCount - 1);
-
-
-
-					platformNode = platform.transform.GetChild (1);
-					sizeofCurrentPlane = platform.GetComponent<Renderer> ().bounds.size;
-
-					if (lastdirection == "vert+")
-						temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x + sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
-					else if (lastdirection == "vert-")
-						temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x - sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
-					else if (lastdirection == "hort-")
-						temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z), currentHallway [cornerPiece].localRotation);
-					else if (lastdirection == "hort+")
-						temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z), currentHallway [cornerPiece].localRotation);
-
-					temp.transform.SetParent (this.transform);
-					roomNodeOffsets++;
-					usedRoomDirections.Add (direction);
+					addCorner(currentHallway);
 				}
 			}
-			usedRoomDirections.Clear ();
-			roomNodeOffsets = 0;
-			platform = transform.GetChild (transform.childCount - 1);
+			addRoom();
 
-			sizeofCurrentPlane = room1Plane.GetComponent<Renderer> ().bounds.size;
-
-			Debug.Log (platform.position);
-			Debug.Log (platform.GetComponent<Renderer> ().bounds.size.x / 2f);
-			Debug.Log (sizeofCurrentPlane);
-			sizeofCurrentPlane = (new Vector3 (sizeofCurrentPlane.x + platform.GetComponent<Renderer> ().bounds.size.x / 2f + 5f, sizeofCurrentPlane.y, sizeofCurrentPlane.z + platform.GetComponent<Renderer> ().bounds.size.z / 2f + 5f));
-			Debug.Log (sizeofCurrentPlane);
-			if (direction == "vert+")
-				temp = Instantiate (Room1, new Vector3 (platform.position.x + sizeofCurrentPlane.x / 2f, platform.position.y, platform.position.z), Quaternion.identity);
-			else if (direction == "vert-")
-				temp = Instantiate (Room1, new Vector3 (platform.position.x - sizeofCurrentPlane.x / 2f, platform.position.y, platform.position.z), Quaternion.identity);
-			else if (direction == "hort-")
-				temp = Instantiate (Room1, new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z / 2f), Quaternion.identity);
-			else if (direction == "hort+")
-				temp = Instantiate (Room1, new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z / 2f), Quaternion.identity);
-
-			temp.transform.SetParent (this.transform);
-
-			randomChoice = Random.Range (0, 3);
-
-			//set direction for platform after room
-
-			lastdirection = direction;
-
-			if (direction.Contains ("hort") && randomChoice == 0) {
-
-				hallwayIndex = 0;
-
-				if (direction.Contains ("hort+")) {
-					cornerPiece = 5;
-				} else {
-					cornerPiece = 4;
-				}
-
-				direction = "vert+";
-
-			} else if (direction.Contains ("hort") && randomChoice == 1) {
-
-				hallwayIndex = 0;
-
-				if (direction.Contains ("hort+")) {
-					cornerPiece = 3;
-				} else {
-					cornerPiece = 2;
-				}
-
-				direction = "vert-";
-			} else if (direction.Contains ("hort") && randomChoice == 2) {
-				cornerPiece = 1;
-			} else if (direction.Contains ("vert") && randomChoice == 0) {
-
-				hallwayIndex = 1;
-
-				if (direction.Contains ("vert+")) {
-					cornerPiece = 2;
-				} else {
-					cornerPiece = 4;
-				}
-
-				direction = "hort+";
-
-			} else if (direction.Contains ("vert") && randomChoice == 1) {
-
-				hallwayIndex = 1;
-
-
-				if (direction.Contains ("vert+")) {
-					cornerPiece = 3;
-				} else {
-					cornerPiece = 5;
-				}
-
-				direction = "hort-";
-			} else if (direction.Contains ("vert") && randomChoice == 2) {
-				//do nothing, keep going in same direction
-				cornerPiece = 0;
-			}
 		}
 	}
 
@@ -272,8 +91,39 @@ public class randomMap : MonoBehaviour
 
 	}
 
+	void addPanel(Transform[] currentHallway){
+
+		sizeofCurrentPlane = platform.transform.GetComponent<Renderer> ().bounds.size;
+
+		//if sizeofCurrentPlane is a room half it
+		if (sizeofCurrentPlane.x > 10) {
+
+			sizeofCurrentPlane = sizeofCurrentPlane / 2f;
+			if (direction == "vert+")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x + sizeofCurrentPlane.x + 5f, platform.position.y, platform.position.z), Quaternion.identity);
+			else if (direction == "vert-")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x - sizeofCurrentPlane.x - 5f, platform.position.y, platform.position.z), Quaternion.identity);
+			else if (direction == "hort-")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z + 5f), currentHallway [hallwayIndex].localRotation);
+			else if (direction == "hort+")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z - 5f), currentHallway [hallwayIndex].localRotation);
+			temp.transform.SetParent (this.transform);
+		} else {
+			if (direction == "vert+")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x + sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
+			else if (direction == "vert-")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x - sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
+			else if (direction == "hort-")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z), currentHallway [hallwayIndex].localRotation);
+			else if (direction == "hort+")
+				temp = Instantiate (currentHallway [hallwayIndex], new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z), currentHallway [hallwayIndex].localRotation);
+			temp.transform.SetParent (this.transform);
+		}
+	}
+
 	void getDirection(){
 
+		randomChoice = Random.Range (0, 3);
 
 		lastdirection = direction;
 
@@ -332,5 +182,53 @@ public class randomMap : MonoBehaviour
 		}
 
 
+	}
+
+	void addCorner(Transform[] currentHallway){
+
+		//add the last piece of tile here
+		platform = transform.GetChild (transform.childCount - 1);
+
+
+
+		sizeofCurrentPlane = platform.GetComponent<Renderer> ().bounds.size;
+
+		if (lastdirection == "vert+")
+			temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x + sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
+		else if (lastdirection == "vert-")
+			temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x - sizeofCurrentPlane.x, platform.position.y, platform.position.z), Quaternion.identity);
+		else if (lastdirection == "hort-")
+			temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z), currentHallway [cornerPiece].localRotation);
+		else if (lastdirection == "hort+")
+			temp = Instantiate (currentHallway [cornerPiece], new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z), currentHallway [cornerPiece].localRotation);
+
+		temp.transform.SetParent (this.transform);
+		roomNodeOffsets++;
+		if (!usedRoomDirections.Contains(direction))
+			usedRoomDirections.Add (direction);
+		for (int m = 0; m < usedRoomDirections.Count; m++)
+			Debug.Log("used room direction: " + usedRoomDirections[m]);
+	}
+
+	void addRoom(){
+		usedRoomDirections.Clear ();
+		roomNodeOffsets = 0;
+		platform = transform.GetChild (transform.childCount - 1);
+
+		sizeofCurrentPlane = room1Plane.GetComponent<Renderer> ().bounds.size;
+
+		sizeofCurrentPlane = (new Vector3 (sizeofCurrentPlane.x + platform.GetComponent<Renderer> ().bounds.size.x / 2f + 5f, sizeofCurrentPlane.y, sizeofCurrentPlane.z + platform.GetComponent<Renderer> ().bounds.size.z / 2f + 5f));
+		if (direction == "vert+")
+			temp = Instantiate (Room1, new Vector3 (platform.position.x + sizeofCurrentPlane.x / 2f, platform.position.y, platform.position.z), Quaternion.identity);
+		else if (direction == "vert-")
+			temp = Instantiate (Room1, new Vector3 (platform.position.x - sizeofCurrentPlane.x / 2f, platform.position.y, platform.position.z), Quaternion.identity);
+		else if (direction == "hort-")
+			temp = Instantiate (Room1, new Vector3 (platform.position.x, platform.position.y, platform.position.z + sizeofCurrentPlane.z / 2f), Quaternion.identity);
+		else if (direction == "hort+")
+			temp = Instantiate (Room1, new Vector3 (platform.position.x, platform.position.y, platform.position.z - sizeofCurrentPlane.z / 2f), Quaternion.identity);
+
+		temp.transform.SetParent (this.transform);
+
+		getDirection();
 	}
 }
