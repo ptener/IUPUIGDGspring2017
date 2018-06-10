a7b4aeb0ce52418836f8337ce2f26b7b1dc0c071
Patrick Tener <ptener@umail.iu.edu>
2018-06-10 10:07:56 -0400

    Popcon 3D Model Gallery

---
+++ Assets/Scripts/BeginPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginPlay : MonoBehaviour {
+
+    public void StartGame() {
+        SceneManager.LoadScene("VisualNovelBrian");
+    }
+
+    public void ViewGalery()
+    {
+        SceneManager.LoadScene("3DModels");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
